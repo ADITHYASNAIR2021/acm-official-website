e550dabaf475889f4730d9bc2536fa9a3b25708a
Advait Suresh <advaitsuresh2002@gmail.com>
2022-11-22 23:26:05 +0530

    Updated FacultyManagement Section

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
+import FacultyManagement from "./../components/FacultyManagement";
 
 function HomePage() {
-  return (
-    <div>
-      <h1 className='text-2xl font-bold '>HomePage</h1>  
-
-
-    </div>
-  )
+    return (
+        <div>
+            <h1 className="text-2xl font-bold p-[0.1]">HomePage</h1>
+            <div className="Faculty-section min-w-screen min-h-screen font-RedRose font-bold leading-8">
+                <FacultyManagement />
+            </div>
+        </div>
+    );
 }
 
-export default HomePage
+export default HomePage;
