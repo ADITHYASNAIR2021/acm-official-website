c9ac1c62dfb94ecc7389d90f8bf47050a91ff930
Akash <akashharikumar2013@gmail.com>
2022-11-27 20:34:31 +0530

    Implemented About Us section, Added button Function feature to button component

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import FacultyManagement from "./../components/FacultyManagement";
-
+import AboutUs from "../components/About-Us/AboutUs";
 function HomePage() {
     return (
         <div>
             <h1 className="text-2xl font-bold p-[0.1]">HomePage</h1>
             <div className="Faculty-section min-w-screen min-h-screen font-RedRose font-bold leading-8">
+                <AboutUs />
                 <FacultyManagement />
             </div>
         </div>
