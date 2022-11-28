6bc6bf7003df5f252f860102bebd0d30620a7ee7
gautamk01 <charmingdeveloper@protonmail.com>
2022-11-28 17:06:56 +0530

    Done work

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
 import Head from "next/head";
 import HomePage from "./HomePage";
 import { useRef } from "react";
-
 export default function Home() {
 
   const myRef = useRef()
- 
+
   return (
     <div>
 
-<div ref={myRef}>
-  <HomePage  />
-</div>
-      
+      <div ref={myRef}>
+        <HomePage />
+      </div>
+
     </div>
   );
 }
