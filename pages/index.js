2f5e30d6c44761aceb36155c00c3a98d5f8f82ae
Akhil <akhilskumar120@gmail.com>
2023-02-01 12:40:24 +0530

    Updated title and description

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,17 @@ import Head from "next/head";
 import HomePage from "./HomePage";
 import { useRef } from "react";
 export default function Home() {
-
-  const myRef = useRef()
+  const myRef = useRef();
 
   return (
     <div>
-
+      <Head>
+        <title>ACM Student Chapter - Amrita Amritapuri Campus</title>
+        <meta name="description" content="The Association for Computing Machinery (ACM) student chapter at Amrita Vishwa Vidyapeetham Amritapuri Campus provides students with opportunities to enhance their coding skills in various domains including Machine Learning, Web Development, Competitive Programming, Game Development, Blockchain, and Web 3."/>
+      </Head>
       <div ref={myRef}>
         <HomePage />
       </div>
-
     </div>
   );
 }
