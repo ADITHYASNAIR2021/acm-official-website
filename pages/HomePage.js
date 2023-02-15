59eb099e6f63f9c2b10c3eb87e6298cc13b5e057
Akash Harikumar <akashharikumar2013@gmail.com>
2023-02-15 22:50:28 +0530

    Finished Event Page

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -10,7 +10,7 @@ import Footer from "../components/Footer";
 function HomePage() {
     return (
         <div>
-            <Navbar />
+            <Navbar isTransparent={false}/>
             <Landing />
             <AboutUs />
             <OurObj />
