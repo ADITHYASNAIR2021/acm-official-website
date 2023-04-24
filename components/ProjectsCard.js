d69e589c8a67ec45d43baa213f7dede16ebeae8a
Akhil <akhilskumar120@gmail.com>
2023-04-24 23:37:43 +0530

    props update

--- components/ProjectsCard.js
+++ components/ProjectsCard.js
@@ -1,18 +1,20 @@
 import React from "react";
 
 const ProjectsCard = (props) => {
+  const { title, desc, link } = props;
+
   return (
     <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {props.title}
+          {title}
         </h5>
       </a>
       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        {props.desc}
+        {desc}
       </p>
       <a
-        href={props.link}
+        href={link}
         className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         GitHub
