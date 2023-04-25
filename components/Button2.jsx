df93de5939bd864cf479a977ebed1dde9a117a04
Akhil <akhilskumar120@gmail.com>
2023-04-25 22:30:39 +0530

    Why join us updates

--- components/Button2.jsx
+++ components/Button2.jsx
@@ -1,27 +1,21 @@
-/*
-    When passing functions make sure they are in this form:
-    const functionName = ()=>{
-        function content
-    }
- */
-    const Button = ({message, buttonFunction}) => {
-        
-        return (
-            <div className="">
-                {
-                    buttonFunction !=undefined &&
-                    <button className="button2" onClick={()=>{buttonFunction()}}>
-                        {message}
-                    </button>
-                }
-                {
-                    buttonFunction === undefined &&
-                    <button className="button2">
-                        {message}
-                    </button>
-                }
-            </div> 
-         );
-    }
-     
-    export default Button;
+const Button = ({ message, buttonFunction }) => {
+  return (
+    <div>
+      {buttonFunction != undefined && (
+        <button
+          className="button2"
+          onClick={() => {
+            buttonFunction();
+          }}
+        >
+          {message}
+        </button>
+      )}
+      {buttonFunction === undefined && (
+        <button className="button2">{message}</button>
+      )}
+    </div>
+  );
+};
+
+export default Button;
--- pages/JoinTeam.js
+++ pages/JoinTeam.js
@@ -8,109 +8,111 @@ import { Navbar } from "../components/Navbar";
 import Footer from "../components/Footer";
 
 function returnSVG(id) {
-    switch (id) {
-      case "1":
-        return <Intrc className="scale-85 mb-2" />;
-        break;
-      case "2":
-        return <Creative className="scale-85 mb-2" />;
-        break;
-      case "3":
-        return <BigIdea className="scale-85 mb-2" />;
-        break;
-        case "4":
-        return <Intrc className="scale-85 mb-2" />;
-        break;
-      case "5":
-        return <Creative className="scale-85 mb-2" />;
-        break;
-      case "6":
-        return <BigIdea className="scale-85 mb-2" />;
-        break;
-    }
+  switch (id) {
+    case "1":
+      return <Intrc className="scale-85 mb-2" />;
+      break;
+    case "2":
+      return <Creative className="scale-85 mb-2" />;
+      break;
+    case "3":
+      return <BigIdea className="scale-85 mb-2" />;
+      break;
+    case "4":
+      return <Intrc className="scale-85 mb-2" />;
+      break;
+    case "5":
+      return <Creative className="scale-85 mb-2" />;
+      break;
+    case "6":
+      return <BigIdea className="scale-85 mb-2" />;
+      break;
   }
-  const detArr=[
-    {
-        id:"1",
-        title: "Interactive",
-        desc:"  Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
-    },
-    {
-        id:"2",
-        title: "dsfdsfctive",
-        desc:"  Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
-    },
-    {
-        id:"3",
-        title: "Interactive",
-        desc:"  Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
-    },
-    {
-        id:"4",
-        title: "Interactive",
-        desc:"  Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
-    },
-    {
-        id:"5",
-        title: "dsfdsfctive",
-        desc:"  Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
-    },
-    {
-        id:"6",
-        title: "Interactive",
-        desc:"  Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
-    }
-]
+}
+const detArr = [
+  {
+    id: "1",
+    title: "Interactive",
+    desc: "  Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+  },
+  {
+    id: "2",
+    title: "dsfdsfctive",
+    desc: "  Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+  },
+  {
+    id: "3",
+    title: "Interactive",
+    desc: "  Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+  },
+  {
+    id: "4",
+    title: "Interactive",
+    desc: "  Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+  },
+  {
+    id: "5",
+    title: "dsfdsfctive",
+    desc: "  Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+  },
+  {
+    id: "6",
+    title: "Interactive",
+    desc: "  Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+  },
+];
 function JoinTeam() {
-    
   return (
-    <div className=" joinuspage pb-10 min-h-screen">
+    <div className=" joinuspage pb-10 min-h-screen overflow-x-hidden">
       <Navbar />
       <div className="grid first-div place-items-center  text-white">
-        <div className="flex flex-col gap-y-24 xl:w-[90%] mt-5 mb-5">
-          <div className="flex justify-center">
-            <div className=" grid place-items-center text-center ">
-              <div className="grid sm:grid-cols-3 sm:grid-rows-1  grid-rows-3 place-items-center">
-                <div className="grid-rows-2 grid place-items-center sm:place-items-start">
-                <p className="jointext "> JOIN</p>
-                <p className="ourteam "> OUR TEAM</p>
-                </div>
-                <div className="flex items-center justify-center">
-            <Button2 message={"GET IN TOUCH"} />
-          </div>
-                <p className="text-xl break-words pl-12 pr-12 font-light">
-                  The objective of the chapter to promote computer science as a
-                  field of education and foster a sense of innovation and
-                  creativity among computer enthusiasts.
+        <div className="flex flex-col gap-y-24 xl:w-[90%] mt-10 mb-5">
+          <div className="flex lg:grid">
+            <div className="grid grid-rows-3 lg:grid-cols-3 lg:grid-rows-1 place-items-center text-center">
+              <div className="flex lg:grid">
+                <p className="jointext flex items-center justify-center flex-wrap ">
+                  JOIN
+                </p>
+                <p className="ourteam flex items-center justify-center flex-wrap ">
+                  OUR TEAM
                 </p>
               </div>
+              <div className="flex items-center justify-center pb-10 pt-6">
+                <Button2 message={"GET IN TOUCH"} />
+              </div>
+              <p className="text-lg text-gray-300 flex-wrap px-12 text-center font-light lg:text-start">
+                The objective of the chapter to promote computer science as a
+                field of education and foster a sense of innovation and
+                creativity among computer enthusiasts.
+              </p>
             </div>
           </div>
           <div className="flex justify-center">
             <div className=" grid grid-cols-1 sm:grid-cols-3 gap-24 w-3/4 sm:w-4/5">
-            {detArr.map((pgdet) => (
-                    <div className="w-full rounded border-2 border-blue-700 card-gradient" key={pgdet.id}>
-                    <div className="px-6 py-4">
-                      <div className="flex justify-center">
+              {detArr.map((pgdet) => (
+                <div
+                  className="w-full rounded border-2 border-blue-700 card-gradient"
+                  key={pgdet.id}
+                >
+                  <div className="px-6 py-4">
+                    <div className="flex justify-center">
                       {returnSVG(pgdet.id)}
-                      </div>
-                      <div className="flex items-center justify-center">
-                        <ObjRec className="scale-75" />
-                        <div className="font-bold text-xl ml-4 text-center">
-                          {pgdet.title}
-                        </div>
-                      </div>
-                      <p className="text-white text-base text-center font-light">
-                        {pgdet.desc}
-                      </p>
                     </div>
+                    <div className="flex items-center justify-center">
+                      <ObjRec className="scale-75" />
+                      <div className="font-bold text-xl ml-4 text-center">
+                        {pgdet.title}
+                      </div>
+                    </div>
+                    <p className="text-white text-base text-center font-light">
+                      {pgdet.desc}
+                    </p>
                   </div>
-                 ))}
-              
+                </div>
+              ))}
             </div>
           </div>
         </div>
-        
       </div>
       <Footer />
     </div>
