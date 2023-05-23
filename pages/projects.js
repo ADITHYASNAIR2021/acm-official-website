ec35e000c29971a1adc022b3bcfea5658d5d066f
Akhil <akhilskumar120@gmail.com>
2023-05-23 12:57:14 +0530

    Updated projects

--- pages/projects.js
+++ pages/projects.js
@@ -23,11 +23,12 @@ const projects = () => {
           </div>
         </div>
         <div className="grid place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 lg:gap-6 p-5 my-2">
-          <ProjectsCard title="ACM Student Management System" desc="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order." link="https://github.com/ACM-Amrita-Amritapuri/acm-official-website"/>
-          <ProjectsCard title="Autodeck" desc="A directory of all the autodrivers around the Vallikavu region" link="https://github.com/Takashi069/autodeck"/>
-          <ProjectsCard title="Noteworthy technology acquisitions 2021" desc="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order." link="https://github.com/ACM-Amrita-Amritapuri/acm-official-website"/>
-          <ProjectsCard title="Noteworthy technology acquisitions 2021" desc="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order." link="https://github.com/ACM-Amrita-Amritapuri/acm-official-website"/>
-          <ProjectsCard title="Noteworthy technology acquisitions 2021" desc="Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order." link="https://github.com/ACM-Amrita-Amritapuri/acm-official-website"/>
+          <ProjectsCard title="ACM TCM" desc="The ultimate platform for student attendance and club analytics, simplifying total club management." link="https://github.com/ACM-Amrita-Amritapuri/ACM-Attendance-Management-System"/>
+          <ProjectsCard title="Autodeck" desc="our one-stop website for easy contact with auto drivers, fare knowledge, and fare splitting." link="https://github.com/Takashi069/autodeck"/>
+          <ProjectsCard title="Apnicareer" desc="The ultimate job search engine and analytics platform for engineers, connecting job seekers and recruiters." link="https://github.com/akhil-s-kumar/apnicareer-webapp"/>
+          <ProjectsCard title="Stitch Craft" desc="Fusing tradition and technology, connecting customers with local tailors for seamless and exquisite craftsmanship." link="https://github.com/kehshiba/stitchcraft"/>
+          <ProjectsCard title="Trouver" desc="Discover local medical facilities in Amritapuri with ease. Find clinics, shops, and contribute information." link="https://github.com/NiranjanNR/Trouver"/>
+          <ProjectsCard title="Cine Vault" desc="Your virtual treasure trove of movies, with trailers, ratings, and genres for the perfect film." link="https://github.com/abhinandarun-02/CineVault"/>
         </div>
       </div>
       <Footer />
